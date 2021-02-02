3938b3abfec559711e015346e14e7626abf985f7
Rise Erpelding <rise@heysparkbox.com>
2021-02-02 16:08:56 -0500

    Test: Submit additional test.

--- src/gilded_rose.spec.js
+++ src/gilded_rose.spec.js
@@ -7,5 +7,11 @@ describe('`updateQuality`', () => {
     expect(standardItem.sell_in).toBe(4);
   });
 
+  it("updates the quality to increase by 1 for Aged Brie", () => {
+    const agedBrie = new Item('Aged Brie', 2, 0);
+    updateQuality([agedBrie]);
+    expect(agedBrie.quality).toBe(1);
+  });
+
   it.todo('This is a good place for a good test!');
 });
